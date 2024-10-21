a404a2b0dcc24902c279d8a028b8fb5c7f44b22e
Nouran <136014827+Nouran247@users.noreply.github.com>
2024-10-21 06:34:50 +0300

    home.js

--- home.js
+++ home.js
@@ -1,47 +1,28 @@
-//PHOTO GALLERY
-
-let currentIndex = 0;
-const images = document.querySelectorAll('.gallery-image');
-
-
-function showNextImage() {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
-}
-
-// ANIMATION FOR THE CONTACT US AND ABOUT US SECTIONS
-setInterval(showNextImage, 3000);
-
+// ANIMATION 
 document.addEventListener("scroll", function() {
-    const contactSection = document.querySelector(".contact-section");
-    const sectionPosition = contactSection.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1.3;
 
-    if (sectionPosition < screenPosition) {
-        contactSection.classList.add("show");
-    }
+    const sectionsToAnimate = [
+        { element: document.querySelector(".contact-section"), className: "show" },
+        { element: document.querySelector(".abtus-section"), className: "show" },
+        { element: document.querySelector(".offers-section"), className: "show" }
+    ];
+
+    sectionsToAnimate.forEach(section => {
+        const sectionPosition = section.element.getBoundingClientRect().top;
+        if (sectionPosition < screenPosition) {
+            section.element.classList.add(section.className);
+        }
+    });
 });
 
-document.addEventListener("scroll", function() {
-    const abtusSection = document.querySelector(".abtus-section");
-    const sectionsPosition = abtusSection.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 1.3;
-
-    if (sectionsPosition < screenPosition) {
-        abtusSection.classList.add("show");
-    }
-});
-
-
-
-//navbar active state
-
+// Navbar 
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll('nav ul li a');
     const sections = document.querySelectorAll('section');
     let activeLink = null;
 
+   
     navLinks.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault();
@@ -52,14 +33,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 behavior: 'smooth'
             });
 
-        
+           
             if (activeLink) activeLink.classList.remove('active');
             this.classList.add('active');
             activeLink = this;
         });
     });
 
-    
     const observer = new IntersectionObserver(
         entries => {
             entries.forEach(entry => {
@@ -74,10 +54,9 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         },
         {
-            threshold: 0.5 
+            threshold: 0.5
         }
     );
 
     sections.forEach(section => observer.observe(section));
 });
-
